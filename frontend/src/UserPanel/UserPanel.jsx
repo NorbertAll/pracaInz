3af1e748898c5dda2aafbd1e64138906d9508a2e
NorbertAll <norbert_gutkowski@interia.pl>
2025-04-11 23:13:31 +0200

    add navigate to logout function

--- frontend/src/UserPanel/UserPanel.jsx
+++ frontend/src/UserPanel/UserPanel.jsx
@@ -64,7 +64,7 @@ export function UserPanel() {
                 setUsername("");
                 console.log("Log out successful!")
                 window.location.reload();
-                navigate(-1)
+
 
             }
         }
