24cb5e7ecd0203df4a3a57fbfd637004329316e6
NorbertAll <norbert_gutkowski@interia.pl>
2025-04-19 11:35:24 +0200

    configure email backend to reset password

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -192,5 +197,19 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+mail=os.environ.get("MAIL")
+mail_pass=os.environ.get("MAIL_PASS")
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER= mail
+EMAIL_HOST_PASSWORD= mail_pass
+DEFAULT_FROM_EMAIL=mail
+EMAIL_USE_SSL=False
+EMAIL_SUBJECT_PREFIX="Password Recovery"
+
 
 CORS_ALLOW_ALL_ORIGINS =True
